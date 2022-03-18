70b606f1caff07b915de913914d0cb2ec3ac0257
Arnold Guevarra <jaimeguevarra22@gmail.com>
2022-03-18 14:22:47 +1100

    @ViewChild

--- src/app/custom-binding/output-binding/output-binding.component.ts
+++ src/app/custom-binding/output-binding/output-binding.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-output-binding',
@@ -8,27 +8,27 @@ import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 export class OutputBindingComponent implements OnInit {
   @Output('evtServCreated') eventServerCreated  = new EventEmitter<{serverName: string, serverContent: string}>();
   @Output('evtBpCreated') eventBlpCreated = new EventEmitter<{serverName: string, serverContent: string}>();
-  //newServerName = '';
-  newServerContent = '';
-
+  //newServerName = ''; //use in 2 way binding with [(ngModel)]
+  //newServerContent = ''; //use in 2 way binding with [(ngModel)]
+  @ViewChild('serverChildContentInput') serverContentInput: ElementRef;
   constructor() { }
 
   ngOnInit(): void {
   }
 
-  onAddedServer(serverNameInput){
+  onAddedServer(serverNameInput: HTMLInputElement){
     console.log(serverNameInput); //retrive the local tag itself; 
 
     this.eventServerCreated.emit({
       serverName: serverNameInput.value,
-      serverContent: this.newServerContent 
+      serverContent: this.serverContentInput.nativeElement.value
     });
   }
 
-  onAddedBlueprint(serverNameInput){
+  onAddedBlueprint(serverNameInput: HTMLInputElement){
     this.eventBlpCreated.emit({
       serverName: serverNameInput.value,
-      serverContent: this.newServerContent 
+      serverContent: this.serverContentInput.nativeElement.value
     });
   }
 
